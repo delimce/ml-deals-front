aea6449e6e5e81e83b65d71e5d042f36351a6102
Luis De Lima <delimce@gmail.com>
2016-12-15 16:22:56 -0400

    adding angular-chart.js libs

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngTouch',
-  'ngSanitize', 'ui.router', 'ngMaterial', 'nvd3', 'app'])
+  'ngSanitize', 'ui.router', 'ngMaterial', 'nvd3', 'app','angularChart','chart.js'])
 
   .config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider,
                     $mdIconProvider) {
