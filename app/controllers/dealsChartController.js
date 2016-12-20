2b097f879a8d73eae9462751bc77bc3f8775e5b4
luis D'Lima <delimce@gmail.com>
2016-12-19 22:26:21 -0400

    model deals charts

--- app/controllers/dealsChartController.js
+++ app/controllers/dealsChartController.js
@@ -4,12 +4,24 @@
         .controller('dealsChartController', function($scope) {
 
 
-            $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-            $scope.series = ['Series A', 'Series B'];
+            $scope.labels = ["Carnaval","Día de los enamorados","Computación","Semana Santa","Día de la Madre",
+                "Especial de Tienda Oficiales","Celulares y teléfonos","Ropa, zapatos, accesorios, relojes, bisutería"
+                ,"Día del Padre","Electronica, audio y video","Industrías","Día del Niño ","Accesorios para vehículos"
+                ,"Celulares y teléfonos","Felices Vacaciones ","Computación","Deportes y fitness","Especial de Tienda Oficiales"
+                ,"Electrodomésticos","Regreso a clases  ","Especial de Tienda Oficiales","Electrodomésticos","Aniversario "
+                ,"Electronica, audio y video","CyberFest","Accesorios para vehículos","Celulares y teléfonos","Deportes y fitness"];
+
+
+
+
+            $scope.series = ['Sucessful item'];
 
             $scope.data = [
-                [65, 59, 80, 81, 56, 55, 40],
-                [28, 48, 40, 19, 86, 27, 90]
+
+                [430.00,23,0,220,0,340,330,450,670,890,0,0,0,0,0,0,0,0,0,0,210.00,0,583.00,0,1,659.00,0,0,0],
+
+
+
             ];
 
 
