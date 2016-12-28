5b3072e65e8e331783a0e3b07d80034516045d4c
luis D'Lima <delimce@gmail.com>
2016-12-28 15:56:18 -0400

    editing  SI and GMV chars with charts.js

--- app/controllers/dealsChartController.js
+++ app/controllers/dealsChartController.js
@@ -1,7 +1,11 @@
 (function () {
     angular
         .module('app')
-        .controller('dealsChartController', function($scope,chartsService) {
+        .controller('dealsChartController', function ($scope, chartsService) {
+
+
+            $scope.chart_si = {}
+            $scope.chart_gmv = {}
 
 
             chartsService.getDataTotals(null).then(function (data) {
@@ -9,27 +13,96 @@
 
                 var cdata = data.content;
                 $scope.labels = [];
-                $scope.si = [];
-                $scope.gmv = [];
-                $scope.series = ['Sucessful item'];
+                $scope.labelsx = [];
+                $scope.chart_si.data = [];
+                $scope.chart_gmv.data = [];
+                $scope.chart_si.series = 'Sucessful item';
+                $scope.chart_gmv.series = 'GMV';
 
 
-                _.each(cdata,function (item) {
+                _.each(cdata, function (item) {
 
                     $scope.labels.push(item.name);
-                    $scope.si.push(item.si);
-                    $scope.gmv.push(item.gmv);
+                    $scope.labelsx.push(item.name.substring(0, 10));
+
+                    $scope.chart_si.data.push(item.si);
+                    $scope.chart_gmv.data.push(item.gmv);
 
                 })
 
+
+                ////SI
+
+                var ctx = $("#chart_si");
+
+                var chart_si = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: $scope.labels,
+                        xLabels:$scope.labelsx,
+                        datasets: [{
+                            label: $scope.chart_si.series,
+                            data: $scope.chart_si.data,
+                            borderWidth: 1
+                        }]
+                    },
+
+                    options: {
+                        title: {
+                            display: true,
+                            text: 'SuccessFul Items'
+                        }
+                    }
+
+                });
+
+
+
+                /////GMV
+                var ctx2 = $("#chart_gmv");
+                var chart_gmv = new Chart(ctx2, {
+                    type: 'bar',
+                    data: {
+                        labels: $scope.labels,
+                        xLabels:$scope.labelsx,
+                        datasets: [{
+                            label: $scope.chart_gmv.series,
+                            data: $scope.chart_gmv.data,
+                            borderWidth: 1
+                        }]
+                    },
+
+                    options: {
+
+                        title: {
+                            display: true,
+                            text: 'GMV'
+                        },
+
+
+                        scales: {
+                            yAxes: [
+                                {
+                                    ticks: {
+                                        callback: function(label, index, labels) {
+                                            return 'Bs.'+label/1000000+'M';
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+
+                    }
+
+                });
+
+
+
             }).catch(function (err) { ////FALLA DEL WEBSERVICES
 
-               console.log(err);
+                console.log(err);
 
             });
-
-
-
 
 
 
